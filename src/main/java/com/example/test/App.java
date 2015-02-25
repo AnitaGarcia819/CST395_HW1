a9d50d77dceda7e125b5df2ca01e5139decc7039
Anita <anitagarcia819@gmail.com>
2015-02-25 22:06:57 +0000

    Added emails

--- src/main/java/com/example/test/App.java
+++ src/main/java/com/example/test/App.java
@@ -1,13 +1,58 @@
 package com.example.test;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+import java.util.List;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    List<String> names;
+
+    public App(List<String> names){
+        this.names = names;
+    }
+
+    public ArrayList<String> createGroup(String targetName) throws Exception {
+        ArrayList<ArrayList<String>> groups = new ArrayList<ArrayList<String>>();
+        ArrayList<String> tmpGroup = new ArrayList<String>();
+        for (String name: this.names) {
+            tmpGroup.add(name);
+            if (tmpGroup.size() == 3) {
+                groups.add(tmpGroup);
+                tmpGroup = new ArrayList<String>();
+            }
+        }
+
+        for (ArrayList<String> group : groups) {
+            if (group.contains(targetName)) {
+                return group;
+            }
+        }
+
+            throw new Exception("target name not found");
+    }
+
+    public static void main( String[] args ) throws Exception {
+        String targetName = "Mariangarcia@csumb.edu";
+        long assignmentNumber = Long.valueOf(1);
+
+        List<String> names = new ArrayList<String>();
+        names.add("mbichay@csumb.edu");
+        names.add("mariangarcia@csumb.edu");
+        names.add("lgarciajimenez@csumb.edu");
+        names.add("savillavicencio@csumb.edu");
+        names.add("mhernandezbucio@csumb.edu");
+        names.add("asorto@csumb.edu");
+        names.add("chmartinez@csumb.edu");
+        names.add("sathya");
+        names.add("katie");
+        Collections.shuffle(names, new Random(assignmentNumber));
+
+        App app = new App(names);
+
+        System.out.print(app.createGroup(targetName));
     }
 }
+
+
